6fa08f91ef4fd62f4cc7cf3c1a8ed5d7da19f4da
DigiDoggy <aituganov.ilja@gmail.com>
2023-09-06 13:04:15 +0300

    ParameterizedTest

--- src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -8,11 +8,34 @@ public class FizzBuzz {
     //If number is NOT divisible by 3 and 5, then print the number
 
     public static String compute(int i) {
-        if(i%3==0){
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+        if (result.isEmpty()){
+            result.append(i);
+        }
+
+        return result.toString();
+    }
+    /*
+    public static String compute(int i) {
+
+        if((i%3==0) && (i%5==0)){
+            return "FizzBuzz";
+        }
+        else if(i%3==0){
             return "Fizz";
         } else if (i%5==0) {
             return "Buzz";
+        }else {
+            return Integer.toString(i);
         }
-        return null;
     }
+     */
+
 }
